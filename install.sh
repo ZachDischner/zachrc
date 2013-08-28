b986782c2cff56134667b0af29f282738c0d695d
ZachDischner <zach.dischner@gmail.com>
2013-08-28 12:51:33 -0600

    Added keychains and VPN setup

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@ chmod a+x -R ./
 cp ~/.zshrc ~/.zshrc_back
 cp ./.zshrc_copy_to_home_dir ~/.zshrc
 chmod a+x ~/.zshrc
+
+echo "Now go import the VPN files included in .zachrc/VPN,  using the Network utility,if you're using a mac"
+echo "Can also extract the encrypted zip file with keychains in them and move to ~/Library/Keychains"
